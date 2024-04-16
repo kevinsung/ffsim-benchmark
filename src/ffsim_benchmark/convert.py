1e82ac32f8bc9056091f73111a31cb41b834d6d5
Kevin J. Sung <kevjsung@umich.edu>
2024-04-16 09:01:24 -0400

    add conversion functions

--- src/ffsim_benchmark/convert.py
+++ src/ffsim_benchmark/convert.py
@@ -8,9 +8,12 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
+import fqe
+import numpy as np
 import openfermion as of
 
 import ffsim
+from ffsim_benchmark.convert_fqe import fqe_to_pyscf, pyscf_to_fqe_wf
 
 
 def ffsim_op_to_openfermion_op(operator: ffsim.FermionOperator) -> of.FermionOperator:
@@ -19,3 +22,17 @@ def ffsim_op_to_openfermion_op(operator: ffsim.FermionOperator) -> of.FermionOpe
         openfermion_term = tuple((2 * orb + spin, action) for action, spin, orb in term)
         op_openfermion += of.FermionOperator(openfermion_term, coeff)
     return op_openfermion
+
+
+def ffsim_vec_to_fqe_wfn(
+    vec: np.ndarray, norb: int, nelec: tuple[int, int]
+) -> fqe.wavefunction.Wavefunction:
+    return pyscf_to_fqe_wf(
+        vec.reshape(*ffsim.dims(norb, nelec)), norbs=norb, nelec=nelec
+    )
+
+
+def fqe_wfn_to_ffsim_vec(
+    wfn: fqe.wavefunction.Wavefunction, nelec: tuple[int, int]
+) -> fqe.wavefunction.Wavefunction:
+    return fqe_to_pyscf(wfn, nelec).reshape(-1)
