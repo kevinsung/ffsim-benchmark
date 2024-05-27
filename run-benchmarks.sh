a0d9bcba07d17f0804bec9a4084dd5d6f8894338
Kevin J. Sung <kevjsung@umich.edu>
2024-05-26 20:26:52 -0400

    skip benchmarks with existing data

--- run-benchmarks.sh
+++ run-benchmarks.sh
@@ -2,6 +2,6 @@
 
 latest_tag=$(curl -s "https://api.github.com/repos/qiskit-community/ffsim/tags" | jq -r ".[0].name")
 echo "Latest tag: $latest_tag"
-asv run $latest_tag..main
-asv run "--tags='v[0-9]*.[0-9]*.[0-9]*' --no-walk"
+asv run --skip-existing $latest_tag..main
+asv run --skip-existing "--tags='v[0-9]*.[0-9]*.[0-9]*' --no-walk"
 asv gh-pages
