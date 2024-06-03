fdd7837728b6ba79c1b67db1e2db6cfdc8fdc556
Kevin J. Sung <kevjsung@umich.edu>
2024-06-03 19:36:20 -0400

    max 10 versions

--- run-benchmarks.sh
+++ run-benchmarks.sh
@@ -5,5 +5,5 @@ echo "Latest tag: $latest_tag"
 # asv run 762f435f..91ff7433
 # asv run $latest_tag..main
 asv run main^!
-asv run "--tags='v[0-9]*.[0-9]*.[0-9]*' --no-walk"
+asv run "--tags='v[0-9]*.[0-9]*.[0-9]*' --no-walk --max-count=10"
 asv gh-pages
