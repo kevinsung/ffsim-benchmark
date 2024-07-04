5f18b90f0f47852fa069dae356f18435055fc9f2
Kevin J. Sung <kevjsung@umich.edu>
2024-07-04 08:44:37 -0400

    update run script

--- run-benchmarks.sh
+++ run-benchmarks.sh
@@ -2,11 +2,11 @@
 
 latest_tag=$(curl -s "https://api.github.com/repos/qiskit-community/ffsim/tags" | jq -r ".[0].name")
 echo "Latest tag: $latest_tag"
-# asv run 762f435f..91ff7433
 # asv run $latest_tag..main
-asv run --bench "_openfermion" main^!
-asv run --bench "_fqe" main^!
-asv run --bench "_qiskit" main^!
-asv run --bench "*_ffsim" main^!
-asv run --bench "*_ffsim" "--tags='v[0-9]*.[0-9]*.[0-9]*' --no-walk --max-count=10"
+# asv run --bench "_openfermion" main^!
+# asv run --bench "_fqe" main^!
+# asv run --bench "_qiskit" main^!
+# asv run --bench "_ffsim" 762f435f..91ff7433
+asv run --bench "_ffsim" main^!
+asv run --bench "_ffsim" "--max-count=20 --tags='v[0-9]*.[0-9]*.[0-9]*' --no-walk"
 asv gh-pages
