f191fb4c430348f93e5dcbc717c738892f014345
Kevin J. Sung <kevjsung@umich.edu>
2025-12-17 14:58:40 -0500

    update run-benchmarks-for-paper.sh

--- scripts/run-benchmarks-for-paper.sh
+++ scripts/run-benchmarks-for-paper.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-asv run --bench "FermionOperatorBenchmark" v0.0.47^!
-asv run --bench "OperatorActionBenchmark" v0.0.47^!
-asv run --bench "OrbitalRotationBenchmark" v0.0.47^!
-asv run --bench "QuadHamEvoBenchmark" v0.0.47^!
-asv run --bench "TrotterBenchmark" v0.0.47^!
+# uv run asv run --bench "FermionOperatorBenchmark" v0.0.64^!
+# uv run asv run --bench "OperatorActionBenchmark" v0.0.64^!
+uv run asv run --bench "MolecularHamiltonianActionRealBenchmark" v0.0.64^!
+uv run asv run --bench "OrbitalRotationBenchmark" v0.0.64^!
+uv run asv run --bench "QuadHamEvoBenchmark" v0.0.64^!
+uv run asv run --bench "TrotterBenchmark" v0.0.64^!
