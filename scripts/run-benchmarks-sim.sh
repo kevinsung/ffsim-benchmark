e816452c57d63ddc1f24f2978a9866d3d3043030
Kevin J. Sung <kevjsung@umich.edu>
2026-03-12 06:44:04 -0400

    update run script ffsim version

--- scripts/run-benchmarks-sim.sh
+++ scripts/run-benchmarks-sim.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-uv run asv run --config asv.conf.json --bench "TrotterBenchmark" v0.0.69^!
-uv run asv run --config asv.conf.multi.json --bench "TrotterBenchmark" v0.0.69^!
-uv run asv run --config asv.conf.json --bench "QuadHamEvoBenchmark" v0.0.69^!
-uv run asv run --config asv.conf.multi.json --bench "QuadHamEvoBenchmark" v0.0.69^!
-uv run asv run --config asv.conf.json --bench "MolecularHamiltonianActionRealBenchmark" v0.0.69^!
-uv run asv run --config asv.conf.multi.json --bench "MolecularHamiltonianActionRealBenchmark" v0.0.69^!
+uv run asv run --config asv.conf.json --bench "QuadHamEvoBenchmark" v0.0.70^!
+uv run asv run --config asv.conf.multi.json --bench "QuadHamEvoBenchmark" v0.0.70^!
+uv run asv run --config asv.conf.json --bench "TrotterBenchmark" v0.0.70^!
+uv run asv run --config asv.conf.multi.json --bench "TrotterBenchmark" v0.0.70^!
+uv run asv run --config asv.conf.json --bench "MolecularHamiltonianActionRealBenchmark" v0.0.70^!
+uv run asv run --config asv.conf.multi.json --bench "MolecularHamiltonianActionRealBenchmark" v0.0.70^!
 
 # uv run asv run --bench "FermionOperatorBenchmark" v0.0.69^!
 # uv run asv run --bench "OperatorActionBenchmark" v0.0.69^!
