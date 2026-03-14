f2366bca667251a828c603b2a19d11301a6629ab
Kevin J. Sung <kevjsung@umich.edu>
2026-03-13 20:55:02 -0400

    update scripts

--- scripts/run-benchmarks-for-paper.sh
+++ scripts/run-benchmarks-for-paper.sh
@@ -8,3 +8,5 @@ uv run asv run --config asv.conf.json --bench "TrotterBenchmark" v0.0.70^!
 uv run asv run --config asv.conf.multi.json --bench "TrotterBenchmark" v0.0.70^!
 uv run asv run --config asv.conf.json --bench "MolecularHamiltonianActionRealBenchmark" v0.0.70^!
 uv run asv run --config asv.conf.multi.json --bench "MolecularHamiltonianActionRealBenchmark" v0.0.70^!
+uv run asv run --config asv.conf.json --bench "SampleSlaterBenchmarkReal" v0.0.70^!
+uv run asv run --config asv.conf.json --bench "FermionOperatorBenchmark" v0.0.70^!
--- scripts/run-benchmarks-trotter.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-uv run asv run --config asv.conf.json --bench "TrotterBenchmark" v0.0.69^!
-uv run asv run --config asv.conf.multi.json --bench "TrotterBenchmark" v0.0.69^!
