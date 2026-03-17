26564415f8b29a64c3d59a5493c4247d7b2f48e6
Kevin J. Sung <kevjsung@umich.edu>
2026-03-16 20:48:19 -0400

    upgrade ffsim version

--- scripts/run-benchmarks-for-paper.sh
+++ scripts/run-benchmarks-for-paper.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-uv run asv run --config asv.conf.json --bench "DiagCoulombEvoBenchmark" v0.0.70^!
-uv run asv run --config asv.conf.multi.json --bench "DiagCoulombEvoBenchmark" v0.0.70^!
-uv run asv run --config asv.conf.json --bench "QuadHamEvoBenchmark" v0.0.70^!
-uv run asv run --config asv.conf.multi.json --bench "QuadHamEvoBenchmark" v0.0.70^!
-uv run asv run --config asv.conf.json --bench "TrotterBenchmark" v0.0.70^!
-uv run asv run --config asv.conf.multi.json --bench "TrotterBenchmark" v0.0.70^!
-uv run asv run --config asv.conf.json --bench "MolecularHamiltonianActionRealBenchmark" v0.0.70^!
-uv run asv run --config asv.conf.multi.json --bench "MolecularHamiltonianActionRealBenchmark" v0.0.70^!
-uv run asv run --config asv.conf.json --bench "SampleSlaterBenchmarkReal" v0.0.70^!
-uv run asv run --config asv.conf.json --bench "FermionOperatorBenchmark" v0.0.70^!
+uv run asv run --config asv.conf.json --bench "DiagCoulombEvoBenchmark" v0.0.71^!
+uv run asv run --config asv.conf.multi.json --bench "DiagCoulombEvoBenchmark" v0.0.71^!
+uv run asv run --config asv.conf.json --bench "QuadHamEvoBenchmark" v0.0.71^!
+uv run asv run --config asv.conf.multi.json --bench "QuadHamEvoBenchmark" v0.0.71^!
+uv run asv run --config asv.conf.json --bench "TrotterBenchmark" v0.0.71^!
+uv run asv run --config asv.conf.multi.json --bench "TrotterBenchmark" v0.0.71^!
+uv run asv run --config asv.conf.json --bench "MolecularHamiltonianActionRealBenchmark" v0.0.71^!
+uv run asv run --config asv.conf.multi.json --bench "MolecularHamiltonianActionRealBenchmark" v0.0.71^!
+uv run asv run --config asv.conf.json --bench "SampleSlaterBenchmarkReal" v0.0.71^!
+uv run asv run --config asv.conf.json --bench "FermionOperatorBenchmark" v0.0.71^!
