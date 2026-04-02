382dca70cd0029ddc5871ea6efaaadaac3e6beed
Kevin J. Sung <kevjsung@umich.edu>
2026-04-01 21:09:14 -0400

    update benchmark ffsim to v0.0.74

--- scripts/run-benchmarks-slater.sh
+++ scripts/run-benchmarks-slater.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-uv run asv run --config asv.conf.json --bench "SampleSlaterBenchmarkReal" v0.0.70^!
+uv run asv run --config asv.conf.json --bench "SampleSlaterBenchmarkReal" v0.0.74^!
