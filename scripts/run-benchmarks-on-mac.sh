8a0f901d002ed7e3442d8ff82f3b4ce72a0e9ae4
Kevin J. Sung <kevjsung@umich.edu>
2026-04-04 10:17:59 -0400

    update mac script

--- scripts/run-benchmarks-on-mac.sh
+++ scripts/run-benchmarks-on-mac.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 uv run asv run --config asv.conf.json --bench "diag_coulomb.DiagCoulombEvoBenchmark.*\(16, 0.5\)" v0.0.74^!
+uv run asv run --config asv.conf.multi.json --bench "diag_coulomb.DiagCoulombEvoBenchmark.*\(16, 0.5\)" v0.0.74^!
 uv run asv run --config asv.conf.json --bench "quad_ham_evo.QuadHamEvoBenchmark.*\(16, 0.5\)" v0.0.74^!
+uv run asv run --config asv.conf.multi.json --bench "quad_ham_evo.QuadHamEvoBenchmark.*\(16, 0.5\)" v0.0.74^!
 uv run asv run --config asv.conf.json --bench "trotter.TrotterBenchmark.*\(16, 0.5\)" v0.0.74^!
+uv run asv run --config asv.conf.multi.json --bench "trotter.TrotterBenchmark.*\(16, 0.5\)" v0.0.74^!
 uv run asv run --config asv.conf.json --bench "mol_ham_action.MolecularHamiltonianActionRealBenchmark.*\(16, 0.5\)" v0.0.74^!
+uv run asv run --config asv.conf.multi.json --bench "mol_ham_action.MolecularHamiltonianActionRealBenchmark.*\(16, 0.5\)" v0.0.74^!
