0dbf2c75a3aaea61c7d51634fea14e3d0b9f2352
Kevin J. Sung <kevjsung@umich.edu>
2026-04-09 11:24:27 -0400

    update ffsim version

--- scripts/run-benchmarks-for-paper-skip-existing.sh
+++ scripts/run-benchmarks-for-paper-skip-existing.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-uv run asv run --skip-existing --config asv.conf.json --bench "DiagCoulombEvoBenchmark" v0.0.74^!
-uv run asv run --skip-existing --config asv.conf.multi.json --bench "DiagCoulombEvoBenchmark" v0.0.74^!
-uv run asv run --skip-existing --config asv.conf.json --bench "QuadHamEvoBenchmark" v0.0.74^!
-uv run asv run --skip-existing --config asv.conf.multi.json --bench "QuadHamEvoBenchmark" v0.0.74^!
-uv run asv run --skip-existing --config asv.conf.json --bench "TrotterBenchmark" v0.0.74^!
-uv run asv run --skip-existing --config asv.conf.multi.json --bench "TrotterBenchmark" v0.0.74^!
-uv run asv run --skip-existing --config asv.conf.json --bench "MolecularHamiltonianActionRealBenchmark" v0.0.74^!
-uv run asv run --skip-existing --config asv.conf.multi.json --bench "MolecularHamiltonianActionRealBenchmark" v0.0.74^!
-uv run asv run --skip-existing --config asv.conf.json --bench "SampleSlaterBenchmarkReal" v0.0.74^!
-uv run asv run --skip-existing --config asv.conf.json --bench "FermionOperatorBenchmark" v0.0.74^!
+uv run asv run --skip-existing --config asv.conf.json --bench "DiagCoulombEvoBenchmark" v0.0.76^!
+uv run asv run --skip-existing --config asv.conf.multi.json --bench "DiagCoulombEvoBenchmark" v0.0.76^!
+uv run asv run --skip-existing --config asv.conf.json --bench "QuadHamEvoBenchmark" v0.0.76^!
+uv run asv run --skip-existing --config asv.conf.multi.json --bench "QuadHamEvoBenchmark" v0.0.76^!
+uv run asv run --skip-existing --config asv.conf.json --bench "TrotterBenchmark" v0.0.76^!
+uv run asv run --skip-existing --config asv.conf.multi.json --bench "TrotterBenchmark" v0.0.76^!
+uv run asv run --skip-existing --config asv.conf.json --bench "MolecularHamiltonianActionRealBenchmark" v0.0.76^!
+uv run asv run --skip-existing --config asv.conf.multi.json --bench "MolecularHamiltonianActionRealBenchmark" v0.0.76^!
+uv run asv run --skip-existing --config asv.conf.json --bench "SampleSlaterBenchmarkReal" v0.0.76^!
+uv run asv run --skip-existing --config asv.conf.json --bench "FermionOperatorBenchmark" v0.0.76^!
--- scripts/run-benchmarks-normal-order.sh
+++ scripts/run-benchmarks-normal-order.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-uv run asv run --config asv.conf.json --bench "FermionOperatorBenchmark" v0.0.74^!
+uv run asv run --config asv.conf.json --bench "FermionOperatorBenchmark" v0.0.76^!
